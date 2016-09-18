b28544d63c7ea8b1652a147c665cf2d345d14401
Michael Billington <michael.billington@gmail.com>
2016-09-18 22:05:10 +1000

    clean out the Chrome app prototype, has been moved to https://github.com/receipt-print-hq/chrome-raw-print

--- chrome-escpos-receipt/main.js
+++
@@ -1,7 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function(data) {
-  // Open main window
-  chrome.app.window.create('index.html',
-    { id: 'main',
-      innerBounds: {width: 1030, height: 704}
-    });
-});
--- chrome-escpos-receipt/vendor/websocket-server/README.md
+++
@@ -1,5 +0,0 @@
-# websocker-server
-
-These files are sourced from the Chrome App '[websocket-server](https://github.com/GoogleChrome/chrome-app-samples/blob/master/samples/websocket-server/index.js)' example.
-
-
